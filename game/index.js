53e0be1206f3f883fa5433fdd4e713cc7e8780da
marisaoppenheim <marisa.oppenheim@yahoo.com>
2018-05-16 16:33:10 -0400

    we got a board

--- game/index.js
+++ game/index.js
@@ -1,29 +1,65 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-
-
-export default function reducer(state = {board: Map(), turn:'X'}, action) {
-  // TODO
-  switch(action.type){
-    case 'MOVE':
-      return {board : state.board.setIn(action.position, action.player), turn: if(action.player === 'X'){
-        return 'O'
-      }else{
-        return 'X'
-      } }
+export default function reducer(state = { board: Map(), turn: "X" }, action) {
+  switch (action.type) {
+    case "MOVE":
+      return {
+        board: state.board.setIn(action.position, action.player),
+        turn: action.player === "X" ? "O" : "X"
+      };
     default:
-    return state
+      return state;
   }
-
-
 }
 
-  //move
-  const MOVE = 'MOVE'
-  const move = ( coordinate, player) => ( {
-    type: MOVE,
-    position: coordinate,
-    player: player
+export const winner = board => {
+  let i = 2;
+  let streakWinner;
+  while (i >= 0) {
+    if (streak(board, [0, i], [1, i], [2, i])) {
+      streakWinner = streak(board, [0, i], [1, i], [2, i]);
+      return streakWinner;
+    } else if (streak(board, [i, 0], [i, 1], [i, 2])) {
+      streakWinner = streak(board, [i, 0], [i, 1], [i, 2]);
+      return streakWinner;
+    } else {
+      streakWinner = streak(board, [i, i], [i, i], [i, i]);
+      return streakWinner;
+    }
+    i--;
   }
+  if (streakWinner === "X") {
+    return "X has won";
+  } else if (streakWinner === "O") {
+    return "O has won";
+  } else if (streakWinner === null) {
+    return null;
+  } else {
+    return "draw";
+  }
+};
 
-)
+export const streak = (board, firstCoord, ...remainingCoords) => {
+  const firstValue = board.getIn(firstCoord);
+
+  const coordArr = [...remainingCoords];
+
+  for (var i = 0; i < coordArr.length; i++) {
+    let checkCoord = board.getIn(coordArr[i]);
+
+    if (checkCoord === firstValue) {
+      continue;
+    } else {
+      return null;
+    }
+    return firstValue;
+  }
+};
+
+//move
+const MOVE = "MOVE";
+export const move = (player, coordinate) => ({
+  type: MOVE,
+  position: coordinate,
+  player: player
+});
